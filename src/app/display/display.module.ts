3714c85d25c7f97bf4e191c9ab3d3ece465f28ab
DatLe <thanhdat3451@outlook.com>
2020-10-19 11:25:50 +0700

    Add create display

--- src/app/display/display.module.ts
+++ src/app/display/display.module.ts
@@ -3,9 +3,10 @@ import { DisplayRoutingModule } from './display-routing.module';
 import { DetailComponent } from './detail/detail.component';
 import { ListingComponent } from './listing/listing.component';
 import { SharedModule } from '../shared/shared.module';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
-  declarations: [DetailComponent, ListingComponent],
+  declarations: [DetailComponent, ListingComponent, CreateComponent],
   imports: [SharedModule, DisplayRoutingModule],
 })
 export class DisplayModule {}
