5e61965408bb063761cbfac811e3f75813a56ee7
DatLe <thanhdat3451@outlook.com>
2020-10-28 13:46:33 +0700

    New display code

--- src/app/display/create/create.component.ts
+++ src/app/display/create/create.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { SnackBarSuccessComponent } from 'src/app/shared/components';
+import { Pagination } from 'src/app/shared/models/pagination.model';
+import { AccountReadDto, ScreensService, ScreenTemplatesService, StoreReadDto, TemplateReadDto, TemplatesService } from 'src/generated';
 
 @Component({
   selector: 'app-create',
@@ -7,5 +14,41 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateComponent implements OnInit {
-  ngOnInit(): void {}
+  form: FormGroup;
+  templates$: any;
+  screens$: any;
+  constructor(
+    private formBuilder: FormBuilder,
+    private displaysService: ScreenTemplatesService,
+    private templatesService: TemplatesService,
+    private screenService: ScreensService,
+    private _snackBar: MatSnackBar
+  ) {
+    this.form = this.formBuilder.group({
+      storeId: ['', Validators.required],
+      uid: ['', Validators.required]
+    });
+  }
+  ngOnInit(): void {
+    this.templates$ = this.templatesService.apiTemplatesGet();
+    this.screens$ = this.screenService.apiScreensGet();
+  }
+
+  create() {
+    // const { storeId, uid } = this.form.value;
+    this.displaysService
+      .apiScreenTemplatesPost({})
+      .pipe(take(1))
+      .subscribe(() => {
+        this._snackBar.openFromComponent(SnackBarSuccessComponent, {
+          verticalPosition: 'top',
+          horizontalPosition: 'end',
+          panelClass: 'mat-snack-bar-success',
+          data: {
+            title: 'Success !',
+            message: `Create "${name}" successfully`,
+          },
+        });
+      });
+  }
 }
